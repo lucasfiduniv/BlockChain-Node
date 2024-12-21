ac38e77e1d3625e4815bbabdb31387ddc41a1a9d
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-12-21 13:23:14 -0300

    fix: 🐛 criando um gerador de transacoes para testar

--- generateKeys.js
+++ generateKeys.js
@@ -1,11 +1,32 @@
 const EC = require("elliptic").ec;
+const crypto = require("crypto");
+
 const ec = new EC("secp256k1");
 
-// Gera um novo par de chaves
-const key = ec.genKeyPair();
-const privateKey = key.getPrivate("hex");
-const publicKey = key.getPublic("hex");
 
-// Imprime a chave privada e o endereço público
-console.log("Nova Chave Privada:", privateKey);
-console.log("Novo Endereço Público (fromAddress):", publicKey);
+const privateKey = "4f3edf983ac636a65a842ce7c78d9aa706d3b113bce36f8e4d8e6e52c4d8fa73";
+const key = ec.keyFromPrivate(privateKey, "hex");
+
+
+const fromAddress = key.getPublic("hex");
+const toAddress = "04f4baddfa5c4820bdb93c9c36f2c78692e4d33c28a2b780df72b6e949d9b504ac";
+const amount = 50;
+
+
+const transactionData = fromAddress + toAddress + amount;
+const transactionHash = crypto.createHash("sha256").update(transactionData).digest("hex");
+
+
+const signature = key.sign(transactionHash, "base64").toDER("hex");
+
+const transaction = {
+  type: "TRANSACTION",
+  transaction: {
+    fromAddress: fromAddress,
+    toAddress: toAddress,
+    amount: amount,
+    signature: signature,
+  },
+};
+
+console.log(JSON.stringify(transaction, null, 2));
